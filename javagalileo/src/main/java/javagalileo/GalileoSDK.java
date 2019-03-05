a8451b9d042b1de6cb994d233170a8d49a0c343c
Zhang Weijia <weijiz@outlook.com>
2019-03-05 20:13:04 +0800

    init

---
+++ javagalileo/src/main/java/javagalileo/GalileoSDK.java
@@ -0,0 +1,18 @@
+package javagalileo;
+
+public class GalileoSDK { // Save as HelloJNI.java
+    static {
+        System.loadLibrary("hello"); // Load native library hello.dll (Windows) or libhello.so (Unixes)
+                                     // at runtime
+                                     // This library contains a native method called sayHello()
+    }
+
+    // Declare an instance native method sayHello() which receives no parameter and
+    // returns void
+    private native void sayHello();
+
+    // Test Driver
+    public static void main(String[] args) {
+        new GalileoSDK().sayHello(); // Create an instance and invoke the native method
+    }
+}
