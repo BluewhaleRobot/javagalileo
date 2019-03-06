620cd660a31f959ddee6e9ceb7188f883cd8a985
Zhang Weijia <weijiz@outlook.com>
2019-03-06 19:18:36 +0800

    add wrapper

---
+++ JNIGalileoWrapper/JNIGalileoWrapper/JNIGalileoWrapper.h
@@ -0,0 +1,7 @@
+#ifndef __JNI_GALILEO_WRAPPER_H__
+#define __JNI_GALILEO_WRAPPER_H__
+#include <jni.h>
+#include <javagalileo_GalileoSDK.h>
+#include "GalileoSDK.h"
+#endif // !__JNI_GALILEO_WRAPPER_H__
+
